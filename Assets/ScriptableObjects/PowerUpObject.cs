a539d80c31403eef21852f648b75c592e916fbe9
Joshua Albe <joshjalbe@gmail.com>
2021-05-12 11:37:16 -0500

    Added scriptable objects

---
+++ Assets/ScriptableObjects/PowerUpObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New PowerUp", menuName = "PowerUp")]
+public class PowerUpObject : ScriptableObject
+{
+    public float attackBoost;
+    public float defenseBoost;
+    public float healthBoost;
+    public float speedBoost;
+
+}
---
+++ Assets/ScriptableObjects/SpawnManagerScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Spawn Manager", menuName = "Spawn Manager")]
+
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+
+
+    public string prefabName;
+    public int spawnCount;
+    public float timeBetweenSpawns;
+
+}
